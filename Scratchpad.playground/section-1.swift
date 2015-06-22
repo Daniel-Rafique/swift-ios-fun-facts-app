3121dbf177bcd9c9f6335304d87898db64f40517
Daniel Rafique <rafique@tamedo.com>
2015-06-22 11:10:26 +0100

    First commit of fun facts app

---
+++ Scratchpad.playground/section-1.swift
@@ -0,0 +1,32 @@
+
+// Playground - noun: a place where people can play
+
+import UIKit
+
+
+//Swift recap
+
+var someVariable = "aVariable"
+let someConstant = 20
+
+var someVarible = "anotherString"
+
+var fruitsArray: [String] = ["Apples"]
+
+fruitsArray.append("Bananas")
+
+fruitsArray
+
+//Optionals
+
+var optoinalString: String? = "Hello"
+//optionalString = nil
+
+
+var randomNumber = Int(arc4random_uniform(10))
+
+var redColor = UIColor(red: 223/255.0, green: 86/255.0, blue: 94/255.0, alpha: 1.0)
+let colorsArray = [redColor]
+
+
+
